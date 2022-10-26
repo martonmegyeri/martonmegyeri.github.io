d9e2fa73b0f94c6712d59c11da326dc2c3175b98
megyerimarton <megyerimarton@gmail.com>
2022-10-26 21:53:56 +0200

    Create curved line under links, use css variables instead of SASS ones, create mobile style for Heading component

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -1,6 +1,16 @@
 import classnames from 'classnames';
 import NextLink from 'next/link';
-import React, { forwardRef, HTMLProps, ReactNode } from 'react';
+import React, {
+  ForwardedRef,
+  forwardRef,
+  HTMLProps,
+  ReactNode,
+  RefObject,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
 import styles from './Link.module.scss';
 
 type Props = HTMLProps<HTMLElement> & {
@@ -10,9 +20,16 @@ type Props = HTMLProps<HTMLElement> & {
   external?: boolean;
 };
 
-function Link({ as, children, className = '', href, external, ...rest }: Props, ref: any) {
+function Link({ as, children, className = '', href, external, ...rest }: Props, ref: ForwardedRef<HTMLElement>) {
+  const innerRef = useRef<HTMLElement>(null);
+  const width = useWidth(innerRef);
+
+  useImperativeHandle(ref, () => innerRef.current as HTMLElement);
+
+  console.log('update');
+
   const props = {
-    ref,
+    ref: innerRef as any,
     ...rest,
     className: classnames(styles.link, {
       [className]: !!className,
@@ -23,6 +40,11 @@ function Link({ as, children, className = '', href, external, ...rest }: Props, 
         <span className={styles.visual} aria-hidden="true">
           {children}
         </span>
+        {width && (
+          <svg width={width} height={20} className={styles.svgLine}>
+            <path d={randomizePath(width)} strokeWidth={window.innerWidth / 200} />
+          </svg>
+        )}
       </>
     ),
   };
@@ -40,6 +62,48 @@ function Link({ as, children, className = '', href, external, ...rest }: Props, 
   }
 
   return React.createElement(as || 'button', props, children);
+}
+
+function useWidth(ref: RefObject<HTMLElement>) {
+  const [width, setWidth] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const observer = new ResizeObserver(() => {
+      if (!ref.current) return;
+      setWidth(ref.current.offsetWidth);
+    });
+    observer.observe(ref.current);
+
+    return () => {
+      if (!ref.current) return;
+      observer.unobserve(ref.current as HTMLDivElement);
+    };
+  }, []);
+
+  return width;
+}
+
+function randomizePath(linkWidth: number) {
+  // TODO: mobile sizes
+
+  console.log(linkWidth);
+
+  const moveYMin = 5; // 200 / 5
+  const moveYMax = 12; // 12
+  const curveXMin = 20; // 20
+  const curveXMax = linkWidth;
+  const curveYMin = 5; // 5
+  const curveYMax = 20; // 20
+  const endYMin = 5; // 5
+  const endYMax = 10; // 10
+  const moveY = Math.floor(Math.random() * (moveYMax - moveYMin)) + moveYMin;
+  const curveX = Math.floor(Math.random() * (curveXMax - curveXMin)) + curveXMin;
+  const curveY = Math.floor(Math.random() * (curveYMax - curveYMin)) + curveYMin;
+  const endY = Math.floor(Math.random() * (endYMax - endYMin)) + endYMin;
+
+  return `M5 ${moveY} Q ${curveX} ${curveY} ${linkWidth - 7} ${endY}`;
 }
 
 export default forwardRef(Link);
